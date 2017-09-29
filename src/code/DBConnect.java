1531d8e6d74630f0a87a4ef623bf1f25885a507f
junjiegao <jgao@student.tgm.ac.at>
2017-09-29 10:27:42 +0200

    Insert

--- src/code/DBConnect.java
+++ src/code/DBConnect.java
@@ -275,7 +275,7 @@ public class DBConnect {
 				+ "'"+id+"',"
 						+ "'"+fname+"',"
 								+ "'"+lname+"',"
-										+ ""+air+","
+										+ "'"+air+"',"
 												+ "'"+flightnr+"');";
 		
 		
